26cb18c5bdf56e58daa921fd35e2dbef065e56ec
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-06-23 14:54:37 +0200

    fix(ingester): metrics data model instead of unstructured dictionary

--- bracelet_sensors_timestream_writer/app.py
+++ bracelet_sensors_timestream_writer/app.py
@@ -2,71 +2,94 @@ import base64
 import json
 import boto3
 import os
+from typing import List, Dict
+from bracelet import BraceletMetric
 
 timestream = boto3.client('timestream-write')
+dynamodb = boto3.client('dynamodb')
 
 
-def record_to_bracelet_data(event):
+def extract_bracelet_metrics(event: Dict[str, List]) -> List[BraceletMetric]:
+    records: List[Dict[str, Dict]] = event.get('Records', [])
     return [
-        json.loads(base64.b64decode(record['kinesis']['data']))
-        for record in event.get('Records', [])
+        BraceletMetric.from_dict(
+            json.loads(
+                base64.b64decode(
+                    record['kinesis']['data']
+                )
+            )
+        )
+        for record in records
     ]
 
 
-def write_to_timestream(data):
-    for r in data:
+def save_bracelet_metrics(metrics: List[BraceletMetric]):
+    for metric in metrics:
         dimensions = [
             {'Name': 'region', 'Value': 'eu-west-1'},
-            {'Name': 'device_id', 'Value': r.get("device_id")},
-            {'Name': 'customer_id', 'Value': r.get("customer_id")}
+            {'Name': 'bracelet_id', 'Value': metric.device_id},
+            {'Name': 'customer_id', 'Value': metric.customer_id}
         ]
         common_attributes = dict(
-            Time=str(r["measured_at"]),
+            Time=str(metric.measured_at),
             TimeUnit="NANOSECONDS",
             Dimensions=dimensions,
         )
         # ID of the bracelet
         device_id = dict(
             MeasureName="device_id",
-            MeasureValue=r.get("device_id"),
+            MeasureValue=str(metric.device_id),
             MeasureValueType='VARCHAR',
         )
         # ID of the customer/user
         customer_id = dict(
             MeasureName="customer_id",
-            MeasureValue=r.get("customer_id"),
+            MeasureValue=str(metric.customer_id),
             MeasureValueType='VARCHAR',
         )
         # Serendipity level of the user
         serendipity = dict(
             MeasureName="serendipity",
-            MeasureValue=str(r.get("serendipity")),
+            MeasureValue=str(metric.serendipity),
+            MeasureValueType="DOUBLE"
+        )
+        # Battery level of the device
+        battery_level = dict(
+            MeasureName="battery_level",
+            MeasureValue=str(metric.battery_level),
             MeasureValueType="DOUBLE"
         )
         # When the measurement has been done
-        measure_time = dict(
+        measurement_time = dict(
             MeasureName="measured_at",
-            MeasureValue=str(r.get("measured_at")),
+            MeasureValue=str(metric.measured_at),
             MeasureValueType="VARCHAR"
         )
+        records = [
+            device_id, customer_id,
+            serendipity, battery_level,
+            measurement_time
+        ]
 
-        records = [device_id, customer_id, serendipity, measure_time]
+        timestream_write(records, common_attributes)
 
-        try:
-            timestream.write_records(
-                DatabaseName=os.getenv("TIMESTREAM_DB"),
-                TableName=os.getenv("TABLE_NAME"),
-                Records=records,
-                CommonAttributes=common_attributes
-            )
-        except timestream.exceptions.RejectedRecordsException as err:
-            print("[WARN] Record has been rejected")
-            print("RejectedRecords: ", err)
-            for rr in err.response["RejectedRecords"]:
-                print("Rejected Index " + str(rr["RecordIndex"]) + ": " + rr["Reason"])
-            print("Other records were written successfully. ")
+
+def timestream_write(records: List[dict], common_attributes: dict):
+    try:
+        timestream.write_records(
+            DatabaseName=os.getenv("TIMESTREAM_DB"),
+            TableName=os.getenv("TIMESTREAM_TABLE"),
+            Records=records,
+            CommonAttributes=common_attributes
+        )
+    except timestream.exceptions.RejectedRecordsException as err:
+        print("[WARN] Record has been rejected")
+        print("RejectedRecords: ", err)
+        for rr in err.response["RejectedRecords"]:
+            print("Rejected Index " + str(rr["RecordIndex"]) + ": " + rr["Reason"])
+        print("Other records were written successfully. ")
 
 
 def lambda_handler(event, context):
-    bracelet_sensor_data = record_to_bracelet_data(event)
-    write_to_timestream(bracelet_sensor_data)
+    bracelet_metrics: List[BraceletMetric] = extract_bracelet_metrics(event)
+    save_bracelet_metrics(bracelet_metrics)
---
+++ bracelet_sensors_timestream_writer/bracelet.py
@@ -0,0 +1,18 @@
+class BraceletMetric:
+    def __init__(self, device_id: str, customer_id: str, serendipity: float, battery_level: float,
+                 measured_at: str):
+        self.device_id = device_id
+        self.customer_id = customer_id
+        self.serendipity = serendipity
+        self.battery_level = battery_level
+        self.measured_at = measured_at
+
+    @classmethod
+    def from_dict(cls, bracelet_data: dict):
+        return cls(
+            device_id=bracelet_data.get("device_id", None),
+            customer_id=bracelet_data.get("customer_id", None),
+            serendipity=bracelet_data.get("serendipity", None),
+            battery_level=bracelet_data.get("battery_level", None),
+            measured_at=bracelet_data.get("measured_at", None)
+        )
