ebcaa06c9f5cb0fe02489c4c2971fc347bc5048d
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-07-01 17:28:55 +0200

    fix(metrics_reader): query string generation by using composable strings

--- metrics_reader/app.py
+++ metrics_reader/app.py
@@ -8,21 +8,43 @@ from timestream_querier import TimestreamQuerier
 
 timestream = boto3.client('timestream-query')
 
+measures_typed_as_double = [
+    "serendipity", "battery_level", "balance"
+]
 
-def build_query(bracelet_id: str, measure_name: str, time_from: str = "1h") -> str:
+
+def query_fields(measure_name: str):
     if measure_name == "":
-        return f"""
-        select *
-        from "{os.getenv("TIMESTREAM_DB")}"."{os.getenv("TIMESTREAM_TABLE")}"
-        where bracelet_id='{bracelet_id}' and time > ago({time_from})
-        """
-    # Specify query for a certain measure since client is not able to know its type
-    elif measure_name == "serendipity" or measure_name == "battery_level" or measure_name == "balance":
-        return f"""
-        select time, measure_name, measure_value::double as measure_value
-        from "{os.getenv("TIMESTREAM_DB")}"."{os.getenv("TIMESTREAM_TABLE")}"
-        where bracelet_id='{bracelet_id}' and measure_name='{measure_name}' and time > ago({time_from})
-        """
+        return "*"
+    elif measure_name in measures_typed_as_double:
+        return "time, measure_name, measure_value::double as measure_value"
+    return ""
+
+
+def build_query(database_name: str, table_name: str, bracelet_id: str, measure_name: str = "",
+                time_from: str = "1h") -> str:
+    """
+    Prepare query for timestream table
+
+    :param database_name: Name of the timestream DB
+    :param table_name: Name of the timestream table
+    :param bracelet_id: Query for metrics belonging to this bracelet
+    :param measure_name: Name of the measure to query
+    :param time_from: TODO
+    :return: Query string
+    """
+    table = f'"{database_name}"."{table_name}"'
+    fields = query_fields(measure_name)
+
+    where = f"bracelet_id='{bracelet_id}' AND time>ago({time_from}) "
+    where += f"AND measure_name='{measure_name}'" if fields != "*" else ""
+
+    query = (
+        f"SELECT {fields} "
+        f"FROM {table} "
+        f"WHERE {where}"
+    )
+    return query
 
 
 def lambda_handler(event: dict, context):
@@ -37,7 +59,7 @@ def lambda_handler(event: dict, context):
     metric = query_string_parameters.get("metric", "")
     time_from = query_string_parameters.get("from", "1d")
 
-    query = build_query(bracelet_id, metric, time_from)
+    query = build_query(os.getenv("TIMESTREAM_DB"), os.getenv("TIMESTREAM_TABLE"), bracelet_id, metric, time_from)
     querier = TimestreamQuerier(timestream)
     result = querier.exec(query)
 
