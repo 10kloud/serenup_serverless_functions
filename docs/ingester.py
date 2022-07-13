69c975a36e85e96b913031e478247ab534960726
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-07-13 10:01:22 +0200

    docs: add missing timestream db in ingester

--- docs/ingester.py
+++ docs/ingester.py
@@ -2,6 +2,10 @@ from diagrams import Diagram
 from diagrams.onprem.client import User
 from diagrams.aws.analytics import KinesisDataStreams
 from diagrams.aws.compute import Lambda
+from diagrams.aws.database import Timestream
 
 with Diagram("", filename="docs/ingester"):
-    User("SerenUp Customer") >> KinesisDataStreams("BraceletMetricIngester") >> Lambda("KinesisToTimestream")
+    User("SerenUp Customer") \
+        >> KinesisDataStreams("BraceletMetricIngester") \
+        >> Lambda("KinesisToTimestream") \
+        >> Timestream("BraceletMetrics")
